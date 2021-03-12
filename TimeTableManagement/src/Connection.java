3e8167a1df06b6ec5d9d3bc8df568940008462b8
UltravioletDarling <iambimsara@gmail.com>
2021-03-12 03:09:50 -0700

    Added libraries

---
+++ TimeTableManagement/src/Connection.java
@@ -0,0 +1,34 @@
+
+import java.sql.DriverManager;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bimsara De Silva
+ */
+public class Connection {
+    
+    public static void main(String []args){
+        java.sql.Connection conn = null;
+        
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/login","root", "");
+            if(conn!=null)
+            {
+                System.out.println("Connected!");
+            }
+        } catch (Exception e)
+        {
+            System.out.println("Failed");
+            
+        }
+    
+    
+    }
+    
+}
